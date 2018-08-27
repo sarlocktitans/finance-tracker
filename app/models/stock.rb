b4f60dad075a036392e4c890f9191207247efca1
rails udemycourse <sarlocktitans@gmail.com>
2018-08-27 13:57:57 +0530

    added portfolio list

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,11 @@
 class Stock < ApplicationRecord
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+	
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
 
+	end
 	def self.new_from_lookup(tickle)
 		begin
 			tickle_search = StockQuote::Stock.quote(tickle);
@@ -13,4 +19,5 @@ class Stock < ApplicationRecord
 	def pricesub(number)
 		number.gsub(",", "")
 	end
+	
 end
