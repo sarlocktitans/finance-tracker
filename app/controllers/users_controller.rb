ede952f20541bd17f83ff36e7da0c897b7b8f8f3
rails udemycourse <sarlocktitans@gmail.com>
2018-08-28 18:48:58 +0530

    App finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,32 @@ class UsersController < ApplicationController
 	end
 
 	def my_friends
+		@friendship = current_user.friends
+	end
+	def search
+		if params[:search_param].blank?
+			flash.now[:danger] = "You have an empty search string"
+		else
+			@users = User.search(params[:search_param])
+			@users = current_user.except_current_user(@users)
+			flash.now[:danger] = "No user found with this Keyword #{params[:search_param]}" if @users.blank?
+		end
+		render partial: 'friends/result'
+	end
 
+	def add_friend
+		@friend = User.find(params[:friend])
+		current_user.friendships.build(friend_id: @friend.id)
+
+		if current_user.save
+			flash[:notice] = "Friend was successfully added"
+		else
+			flash[:danger] = "There is something wrong with the friend request"
+		end
+		redirect_to my_friends_path
+	end
+	def show
+		@user = User.find(params[:id])
+		@user_stocks = @user.stocks
 	end
 end
